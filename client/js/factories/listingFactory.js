673b1eb3feabb59bd75b16b8f1b0ae1cb4649a9a
Matt <meserwinowski@gmail.com>
2018-09-25 22:49:25 -0400

    Modified listingFactory.js to operate with Heroku URL

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -4,16 +4,19 @@ angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
       // console.log(path.join('http://localhost:', pConnection, '/api/listings'));
-      return $http.get('http://localhost:8080/api/listings');
+      // return $http.get('http://localhost:8080/api/listings');
+      return $http.get('https://cen3031-assignments.herokuapp.com/api/listings');
     },
 	
 	  create: function(listing) {
-	    return $http.post('http://localhost:8080/api/listings', listing);
+      // return $http.post('http://localhost:8080/api/listings', listing);
+      return $http.post('https://cen3031-assignments.herokuapp.com/api/listings', listing);
     }, 
 
     delete: function(id) {
       /* Return result of HTTP delete method */
-      return $http.delete('http://localhost:8080/api/listings', id);
+      // return $http.delete('http://localhost:8080/api/listings', id);
+      return $http.delete('https://cen3031-assignments.herokuapp.com/api/listings', id);
     }
   };
   return methods;
